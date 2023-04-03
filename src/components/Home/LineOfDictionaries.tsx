1942f30da30e2eabe19219081f44aec86f60d7bd
evgeniy-shpyra <foksmix256@gmail.com>
2023-04-03 20:29:54 +0300

    add some lines

--- src/components/Home/LineOfDictionaries.tsx
+++ src/components/Home/LineOfDictionaries.tsx
@@ -13,6 +13,19 @@ import {
 const LineOfDictionaries = () => {
     const { scrollYProgress } = useScroll()
 
+    const containerRef = React.useRef(null)
+
+    const [elementTop, setElementTop] = React.useState(0)
+    const [elementBottom, setElementBottom] = React.useState(0)
+
+    React.useEffect(() => {
+        const element = containerRef.current
+        if (element.center != null) {
+            setElementTop(element.offsetTop)
+            setElementBottom(element.offsetTop + element.offsetHeight)
+        }
+    }, [containerRef])
+
     const rightLineX = useTransform(
         scrollYProgress,
         [0, 1],
@@ -28,29 +41,29 @@ const LineOfDictionaries = () => {
     console.log(window.innerWidth)
 
     return (
-        <>
-            <div className="h-[335px] flex flex-col justify-between">
-                <motion.div style={{ x: rightLineX }} className="flex">
-                    {Array.from({ length: 7 }).map((item, index) => (
-                        <img
-                            key={index + 'right'}
-                            src={dictionary1}
-                            className="h-[160px] mx-[7.5px] shadow-secondary rounded-[15px]"
-                        />
-                    ))}
-                </motion.div>
-                <motion.div style={{ x: leftLineX }} className="flex">
-                    {Array.from({ length: 7 }).map((item, index) => (
-                        <img
-                            key={index + 'left'}
-                            src={dictionary1}
-                            className="h-[160px] mx-[7.5px] shadow-secondary rounded-[15px]"
-                        />
-                    ))}
-                </motion.div>
-            </div>
-         
-        </>
+        <div
+            ref={containerRef}
+            className="h-[335px] flex flex-col justify-between"
+        >
+            <motion.div style={{ x: rightLineX }} className="flex">
+                {Array.from({ length: 7 }).map((item, index) => (
+                    <img
+                        key={index + 'right'}
+                        src={dictionary1}
+                        className="h-[160px] mx-[7.5px] shadow-secondary rounded-[15px]"
+                    />
+                ))}
+            </motion.div>
+            <motion.div style={{ x: leftLineX }} className="flex">
+                {Array.from({ length: 7 }).map((item, index) => (
+                    <img
+                        key={index + 'left'}
+                        src={dictionary1}
+                        className="h-[160px] mx-[7.5px] shadow-secondary rounded-[15px]"
+                    />
+                ))}
+            </motion.div>
+        </div>
     )
 }
 
