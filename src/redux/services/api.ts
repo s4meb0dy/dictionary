ffc2914694b2ee0f9c69793d417784c0bb2ead83
evgeniy-shpyra <foksmix256@gmail.com>
2023-05-06 23:12:19 +0300

    Change api url

--- src/redux/services/api.ts
+++ src/redux/services/api.ts
@@ -1,2 +1,4 @@
-export const apiUrl =
-    'https://dictionary-api-production-9561.up.railway.app/api/'
+// export const apiUrl =
+//     'https://dictionary-api-production-9561.up.railway.app/api/'
+
+export const apiUrl = 'http://a92f-188-163-72-22.ngrok-free.app/api'
